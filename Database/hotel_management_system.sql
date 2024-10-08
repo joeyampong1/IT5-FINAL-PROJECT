494c27854cdda6dbd82a0e5ebffc2bdc9264224b
jb-dots <j.dutaro.545524@umindanao.edu.ph>
2024-10-08 22:22:22 +0800

    Add files via upload

--- Database/hotel_management_system.sql
+++ Database/hotel_management_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 07, 2024 at 03:44 PM
+-- Generation Time: Oct 08, 2024 at 04:15 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,77 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `ref` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `postcode` varchar(20) DEFAULT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `nationality` varchar(50) DEFAULT NULL,
+  `idproof` varchar(50) DEFAULT NULL,
+  `idnumber` varchar(50) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`ref`, `name`, `gender`, `postcode`, `mobile`, `email`, `nationality`, `idproof`, `idnumber`, `address`) VALUES
+(1, 'Aldren', 'Female', '8000', '09088184444', 'lanoy@gmail.com', 'Filipino', 'Driving License', '12345', 'Lanoy St. Toril Davao City');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `details`
+--
+
+CREATE TABLE `details` (
+  `floor` int(10) NOT NULL,
+  `roomno` int(200) NOT NULL,
+  `roomtype` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `details`
+--
+
+INSERT INTO `details` (`floor`, `roomno`, `roomtype`) VALUES
+(1, 25, 'Single'),
+(2, 20, 'Double'),
+(4, 21, 'Queen');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register`
+--
+
+CREATE TABLE `register` (
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `contact_number` varchar(15) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `security_question` varchar(255) NOT NULL,
+  `security_answer` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `register`
+--
+
+INSERT INTO `register` (`first_name`, `last_name`, `contact_number`, `email`, `security_question`, `security_answer`, `password`) VALUES
+('John Benedic', 'Dutaro', '09088184444', 'dots@gmail.com', 'Your Best Friend Name', 'russel', '1234'),
+('Aldren', 'Reyes', '09088184444', 'lanoy@gmail.com', 'Your Pet Name', 'Bella', '12345');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -34,7 +105,7 @@ CREATE TABLE `users` (
   `contact` varchar(20) NOT NULL,
   `email` varchar(100) NOT NULL,
   `securityQ` varchar(50) NOT NULL,
-  `securityA` varchar(100) NOT NULL,
+  `securityA` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -43,11 +114,30 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `fname`, `lname`, `contact`, `email`, `securityQ`, `securityA`, `password`) VALUES
-(1, 'dff', 'asdfa', 'fadf', 'dfaf', 'Your Pet Name', 'asd', 'asdasd');
+(1, 'fdd', 'asfd', 'adaf', 'asdfa', 'Your Pet Name', 'asd', 'asdasd');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`ref`);
+
+--
+-- Indexes for table `details`
+--
+ALTER TABLE `details`
+  ADD PRIMARY KEY (`floor`,`roomno`);
+
+--
+-- Indexes for table `register`
+--
+ALTER TABLE `register`
+  ADD PRIMARY KEY (`email`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `users`
@@ -59,6 +149,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `ref` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
