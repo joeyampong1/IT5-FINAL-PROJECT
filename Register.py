e5660606b993d979d7b66c67c28968e3267bfb9c
joeyampong1 <j.ampong.482578@umindanao.edu.ph>
2024-10-10 15:17:18 +0800

    Finish Code of Room.py

--- Register.py
+++ Register.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
 from tkinter import messagebox
-
+import mysql.connector
 
 class Register():
     def __init__(self, root):
